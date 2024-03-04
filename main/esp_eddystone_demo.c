8fb030a8325f2cb10571bc24ace391c2e60e1eff
Zach3292 <Zach3292@hotmail.com>
2024-03-04 11:13:45 -0500

    add led functions

--- main/esp_eddystone_demo.c
+++ main/esp_eddystone_demo.c
@@ -35,12 +35,47 @@
 
 #include "led_strip.h"
 
+// GPIO assignment
+#define LED_STRIP__GPIO 3
+// Numbers of the LED in the strip
+#define LED_STRIP_LED_NUMBERS 12
+// 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
+#define LED_STRIP_RMT_RES_HZ  (10 * 1000 * 1000)
 
 #define GPIO_OUTPUT_IO_0     2 
 #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0))
 
 static const char* DEMO_TAG = "EDDYSTONE_DEMO";
+static const char* TAG = "LED_STRIP";
 
+led_strip_handle_t configure_led(void)
+{
+    // LED strip general initialization, according to your led board design
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = LED_STRIP_BLINK_GPIO,   // The GPIO that connected to the LED strip's data line
+        .max_leds = LED_STRIP_LED_NUMBERS,        // The number of LEDs in the strip,
+        .led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
+        .led_model = LED_MODEL_WS2812,            // LED strip model
+        .flags.invert_out = false,                // whether to invert the output signal
+    };
+
+    // LED strip backend configuration: RMT
+    led_strip_rmt_config_t rmt_config = {
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 0, 0)
+        .rmt_channel = 0,
+#else
+        .clk_src = RMT_CLK_SRC_DEFAULT,        // different clock source can lead to different power consumption
+        .resolution_hz = LED_STRIP_RMT_RES_HZ, // RMT counter clock frequency
+        .flags.with_dma = false,               // DMA feature is available on ESP target like ESP32-C3
+#endif
+    };
+
+    // LED Strip object handle
+    led_strip_handle_t led_strip;
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    ESP_LOGI(TAG, "Created LED strip object with RMT backend");
+    return led_strip;
+}
 
 /* declare static functions */
 static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* param);
@@ -188,6 +223,19 @@ void esp_eddystone_init(void)
     esp_eddystone_appRegister();
 }
 
+void set_led_fill(led_strip_handle_t led_strip, uint8_t red, uint8_t green, uint8_t blue){
+    /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+    for (int i = 0; i < LED_STRIP_LED_NUMBERS; i++) {
+        ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, red, green, blue));
+    }
+    /* Refresh the strip to send data */
+    ESP_ERROR_CHECK(led_strip_refresh(led_strip));
+}
+
+void clear_led(led_strip_handle_t led_strip){
+    ESP_ERROR_CHECK(led_strip_clear(led_strip));
+}
+
 void app_main(void)
 {
     // //zero-initialize the config structure.
@@ -205,7 +253,7 @@ void app_main(void)
     // //configure GPIO with the given settings
     gpio_config(&io_conf);
 
-
+    led_strip_handle_t led_strip = configure_led();
 
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
